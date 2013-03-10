11975ea3421337c41da48e2005b3d6a3b535d011
Alex Poms <alexpoms@gmail.com>
2013-03-10 03:05:38 -0700

    Fix pep8 violations

--- graph_tutorial.py
+++ graph_tutorial.py
@@ -7,6 +7,8 @@ from igraph import Graph
 g = Graph.GRG(50, .2)
 
 #writes a svg file:
-g.write_svg("helloGraph.svg","auto",500,500)
+g.write_svg("helloGraph.svg", "auto", 500, 500)
 
-# write_svg(self, fname, layout='auto', width=None, height=None, labels='label', colors='color', shapes='shape', vertex_size=10, edge_colors='color', font_size=16, *args, **kwds)
+# write_svg(self, fname, layout='auto', width=None, height=None,
+# labels = 'label', colors = 'color', shapes = 'shape', vertex_size = 10,
+# edge_colors = 'color', font_size = 16, *args, **kwds)
