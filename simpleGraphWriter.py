59478883c576b8f7cd102083f32474dfa8e00734
bryan maass <bryan.maass@gmail.com>
2013-03-11 03:47:10 -0700

    used a dict to keep track of edge weights

--- simpleGraphWriter.py
+++ simpleGraphWriter.py
@@ -16,10 +16,10 @@ from xml.dom import minidom
 # this implimentation asusmes that we dont care about the users...
 
 g = Graph()
-
-# XMLpath = "./miniposts.xml"
+print "entering XML"
+# XMLpath = "./miniposts .xml"
 XMLpath = "/Users/bryanmaass/Dropbox/!Winter13/163/"\
-          "final/so-export-2009-08/100posts.xml"
+          "final/so-export-2009-08/posts.xml"
 XMLdoc = minidom.parse(XMLpath)
 rowlist = XMLdoc.getElementsByTagName('row')
 print len(rowlist), "rows parsed"
@@ -27,53 +27,40 @@ print len(rowlist), "rows parsed"
 gen = UniqueIdGenerator()  # check doc on this guy
 # smart add edge, lazy instantiates both vertexes....
 # cant add an edge if one of the verices are not made
-adjList = {}
-
-
-def smart_add_edge(sourceName, targetName):
-    if sourceName not in gen:
-        sourceVertex = {}
-        sourceVertex["uid"] = gen[sourceName]
-        sourceVertex["name"] = sourceName
-        g.add_vertex(sourceVertex)
-        if targetName not in gen:
-            targetVertex = {}
-            targetVertex["uid"] = gen[targetName]
-            targetVertex["name"] = targetName
-            g.add_vertex(targetVertex)
-            g.add_edge(gen[sourceName], gen[targetName])
 
 adjList = {}
 for row in rowlist:
     tags = row.getAttribute("Tags")
     taglist = tags.encode('utf-8').strip("><").split("><")
-    print taglist
     del tags
     for i, source in enumerate(taglist):
         for target in taglist[i + 1:]:
-            print source, target
-            smart_add_edge(source, target)
-            s = set(target).add(source)
-            w = 1/len(taglist)
-            if s in adjList:
-                adjList[s] += w
+            e = (source,target)
+            if e[0] > e[1]:
+                e = (target,source)
+            w = 1.0/len(taglist)
+            if e in adjList:
+                adjList[e] += w
             else:
-                adjList[s] = w
-            print
+                adjList[e] = w
+import operator
+sorted_adjList = sorted(adjList.iteritems(),key=operator.itemgetter(1))
 
-print adjList            
-layout = g.layout('kk')
-width, height = 9000, 6000
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
-ctx = cairo.Context(surface)
-ctx.scale(width, height)
-ctx.rectangle(0, 0, 1, 1)
-ctx.set_source_rgba(0, 0, 0, 0)
-ctx.fill()
-plot = Graph.plot(g, surface, bbox=(width, height), layout=layout)
-plot.background = None
-plot.redraw()
-surface.write_to_png('example.png')
+for line in sorted_adjList:
+    print line
+
+# layout = g.layout('kk')
+# width, height = 9000, 6000
+# surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+# ctx = cairo.Context(surface)
+# ctx.scale(width, height)
+# ctx.rectangle(0, 0, 1, 1)
+# ctx.set_source_rgba(0, 0, 0, 0)
+# ctx.fill()
+# plot = Graph.plot(g, surface, bbox=(width, height), layout=layout)
+# plot.background = None
+# plot.redraw()
+# surface.write_to_png('example.png')
 
 # for i,v in enumerate(g.vs):
 # print i,": ",v['name']
