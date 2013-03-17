720c26ff4857a8285ab2bae68bb10e6f39e67ea2
Alex Poms <alexpoms@gmail.com>
2013-03-17 14:55:19 -0700

    Update settings panel

--- settings_panel.py
+++ settings_panel.py
@@ -4,7 +4,8 @@ import Pyro4
 class SettingsPanel():
     def __init__(self):
         pass
-
+    def run(self):
+        
 
 def launch_settings():
     settings = SettingsPanel()
@@ -13,8 +14,5 @@ def launch_settings():
     ns = Pyro4.locateNS()
     ns.register("info-overflow.settings", settings_uri)
     server = Pyro4.Proxy("PYRONAME:info-overflow.server")
-    server.select_tag("c++")
-    server.select_tag("c#")
-    server.deselect_tag("c++")
     daemon.requestLoop()
     
